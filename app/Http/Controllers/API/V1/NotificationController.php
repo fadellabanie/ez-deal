f9fa004fda121aba210b34c4256c9a69875485ce
fadellabanie <fadel.labanie@gmail.com>
2021-08-02 23:37:04 +0300

    fix noticiation

--- app/Http/Controllers/API/V1/NotificationController.php
+++ app/Http/Controllers/API/V1/NotificationController.php
@@ -2,26 +2,31 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Models\User;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
 use App\Services\Notification;
+use App\Models\NotificationUser;
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\Constants\NotificationResource;
 
 class NotificationController extends Controller
 {
-    public $notification;
-    public function __construct(Notification $notification)
+
+    public function myNotification()
     {
-        $this->notification = $notification;
+        $notifications = NotificationUser::where('user_id',Auth::id())->get();
+
+        return $this->respondWithCollection(NotificationResource::collection($notifications));
+
     }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function send(Request $request)
+    public function sendFCM(Request $request)
     {
         $user =User::find($request->to);
 

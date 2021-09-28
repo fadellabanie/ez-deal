006cbc016747fa4c3a304e099d71b036288987b0
mac <mac@macs-iMac.local>
2021-09-28 16:07:48 +0200

    fix

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -2,58 +2,52 @@
 
 namespace App\Http\Traits;
 
+use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Hash;
 
 trait Elm
 {
     private function login()
     {
-       $nonuce = rand(1000,9999).'-'.rand(1000,9999);
-       $time = time();
-       $timeHash = Hash::make(hash('sha256',time()));
-
-        $url = Hash::make('https://iambeta.elm.sa/authservice/authorize?
+        $nonuce = rand(1000, 9999) . '-' . rand(1000, 9999);
+        $time = time();
+        $file = base_path() . '/iamtest.spname.pem';
+       
+      
+        $url = ('https://iambeta.elm.sa/authservice/authorize?
         scope=openid
         &response_type= id_token
         &response_mode=form_post
         &client_id=16371621
         &redirect_uri=http://ezdeal.net/api/v1/home
-        &nonce=b55224f7-e83d-'.$nonuce.'-451d32666e59
+        &nonce=b55224f7-e83d-' . $nonuce . '-451d32666e59
         &ui_locales=ar
         &prompt=login
-        &max_age='.$time);
+        &max_age=' . $time);
 
-        $private_key = base_path().'';
-        dd($private_key);
-        openssl_sign(
-            $url,
-            $signature,
-            $private_key,
-            $algorithm = OPENSSL_ALGO_SHA1
-        );
-       
+
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 
-        '
-        https://iambeta.elm.sa/authservice/authorize?
+        curl_setopt($ch,CURLOPT_URL,
+        'https://iambeta.elm.sa/authservice/authorize?
         scope=openid
         &response_type= id_token
         &response_mode=form_post
         &client_id=16371621
         &redirect_uri=http://ezdeal.net/api/v1/home
-        &nonce=b55224f7-e83d-'.$nonuce.'-451d32666e59
+        &nonce=b55224f7-e83d-' . $nonuce . '-451d32666e59
         &ui_locales=ar
         &prompt=login
-        &max_age='.$time.'
-        &state='.$url);
-       
+        &max_age=' . $time . '
+        &state=' . Hash::make($url)
+        );
 
-dd($ch);
+
+        dd($ch);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         curl_close($ch);
 
-        return($result);
+        return ($result);
     }
 }

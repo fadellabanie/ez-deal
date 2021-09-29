8180dba658219903600ee2cd3f9a71985cd968c8
mac <mac@macs-iMac.local>
2021-09-29 16:43:29 +0200

    fix

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -2,35 +2,25 @@
 
 namespace App\Http\Traits;
 
-use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Hash;
 
 trait Elm
 {
     private function login()
     {
-        $nonuce = rand(1000, 9999) . '-' . rand(1000, 9999);
+        $nonce = rand(1000, 9999) . '-' . rand(1000, 9999);
         $time = time();
-        $file = base_path() . '/iamtest.spname.key';
 
-        $privateKey = file_get_contents($file);
+        $file = base_path() . '/iamtest.spname.key';  ## File generate from commend documentation
+        $privateKey = file_get_contents($file);       ## Get content for file
 
-        $url = ('https://iambeta.elm.sa/authservice/authorize?
-        scope=openid
-        &response_type= id_token
-        &response_mode=form_post
-        &client_id=16371621
-        &redirect_uri=http://ezdeal.net/api/v1/home
-        &nonce=b55224f7-e83d-' . $nonuce . '-451d32666e59
-        &ui_locales=ar
-        &prompt=login
-        &max_age=' . $time);
+        $url = ('https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type= id_token
+        &response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home
+        &nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time);
 
-        //  dd($privateKey);
-
-        $state = hash_hmac('sha256', $url, $privateKey);
-
-        $ch = curl_init();
+        $state = hash_hmac('sha256', $url, $privateKey); ## Hash url use privatekey
+       
+        $ch = curl_init();                              ## Send request in state hash of url
         curl_setopt(
             $ch,
             CURLOPT_URL,
@@ -40,14 +30,13 @@ trait Elm
         &response_mode=form_post
         &client_id=16371621
         &redirect_uri=http://ezdeal.net/api/v1/home
-        &nonce=b55224f7-e83d-' . $nonuce . '-451d32666e59
+        &nonce=b55224f7-e83d-' . $nonce . '-451d32666e59
         &ui_locales=ar
         &prompt=login
         &max_age=' . $time . '
         &state=' . $state
         );
-        // dd($ch);
-
+       
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);

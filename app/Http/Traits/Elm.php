d5d579e5fa1f381e69f612e59c1debbfcd906acf
mac <mac@macs-iMac.local>
2021-10-06 09:34:48 +0200

    try elm

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -17,15 +17,15 @@ trait Elm
         openssl_pkcs12_read($pfxContent, $certs, $certPassword);
         $privateKey = $certs['pkey'];
 
-        $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-'.$nonce.'-451d32666e59&ui_locales=ar&prompt=login&max_age='.$time;
+        $url = 'https://iam.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-'.$nonce.'-451d32666e59&ui_locales=ar&prompt=login&max_age='.$time;
         $state = hash_hmac('sha256', $url, $privateKey); 
 
-        
+
         $ch = curl_init();                             
         curl_setopt(
             $ch,
             CURLOPT_URL,
-            'https://iambeta.elm.sa/authservice/authorize?
+            'https://iam.elm.sa/authservice/authorize?
             scope=openid
             &response_type=id_token
             &response_mode=form_post

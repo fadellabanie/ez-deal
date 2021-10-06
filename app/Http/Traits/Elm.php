f61e1523918b6cb3d0fcff2a93771912a600f5a8
mac <mac@macs-iMac.local>
2021-10-06 09:29:53 +0200

    try elm

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -15,34 +15,34 @@ trait Elm
         $pfxContent = file_get_contents($file);       ## Get content for file
         $certPassword = '16371621';
         openssl_pkcs12_read($pfxContent, $certs, $certPassword);
-        
-        $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-'.$nonce.'-451d32666e59&ui_locales=ar&prompt=login&max_age='.$time;
-        
-  
-            $PrivateKey = $certs['pkey'];
-             // dd($PrivateKey);
-            //openssl_private_decrypt($url,$state,$PrivateKey);
-            $state = hash_hmac('sha256', $url, $PrivateKey); ## Hash url use privatekey
-            
-        
+
+        $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time;
+
+        $PrivateKey = $certs['pkey'];
+        // dd($PrivateKey);
+        //openssl_private_decrypt($url,$state,$PrivateKey);
+        $state = hash_hmac('sha256', $url, $PrivateKey); ## Hash url use privatekey
+
+
         $ch = curl_init();                              ## Send request in state hash of url
         curl_setopt(
             $ch,
             CURLOPT_URL,
             'https://iambeta.elm.sa/authservice/authorize?
-        scope=openid
-        &response_type= id_token
-        &response_mode=form_post
-        &client_id=16371621
-        &redirect_uri=http://ezdeal.net/api/v1/home
-        &nonce=b55224f7-e83d-' . $nonce . '-451d32666e59
-        &ui_locales=ar
-        &prompt=login
-        &max_age=' . $time . '
-        &state=' . $state
+            scope=openid
+            &response_type= id_token
+            &response_mode=form_post
+            &client_id=16371621
+            &redirect_uri=http://ezdeal.net/api/v1/home
+            &nonce=b55224f7-e83d-' . $nonce . '-451d32666e59
+            &ui_locales=ar
+            &prompt=login
+            &max_age=' . $time . '
+            &state=' . $state
         );
         //dd($ch);
-        curl_setopt($ch, CURLOPT_POST, true);
+      
+        curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         curl_close($ch);

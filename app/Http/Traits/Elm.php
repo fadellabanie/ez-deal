18ee999edb0644e55f04ba015fabe53ecb64337b
mac <mac@macs-iMac.local>
2021-10-10 08:48:18 +0200

    try timezone

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -19,12 +19,12 @@ trait Elm
         openssl_pkcs12_read($pfxContent, $certs, $certPassword);
         $privateKey = $certs['pkey'];
 
-        $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time;
+        $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=https://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time;
 
         $state = hash_hmac('sha256', $url, $privateKey);
           
-        $requestUrl = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=http://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time . '&state=' . $state;
-          //  dd($requestUrl);
+        $requestUrl = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=https://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time . '&state=' . $state;
+            
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $requestUrl);
        // curl_setopt( $ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);

5892ab6359836e6723cf4a65b63d71091c4d7f93
mac <mac@macs-iMac.local>
2021-10-10 09:07:48 +0200

    try timezone

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -29,7 +29,8 @@ trait Elm
         curl_setopt($ch, CURLOPT_URL, $requestUrl);
        // curl_setopt( $ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
         $responseData = curl_exec($ch);
         if (curl_errno($ch)) {
             return curl_error($ch);

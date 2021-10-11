832434256886fc79b893d031f961f4474606d259
mac <mac@macs-iMac.local>
2021-10-11 16:55:52 +0200

    try elm

--- app/Http/Traits/Elm.php
+++ app/Http/Traits/Elm.php
@@ -20,6 +20,7 @@ trait Elm
 
         openssl_pkcs12_read($pfxContent, $certs, $certPassword);
        
+       
         $privateKey = $certs['pkey'];
 
         $url = 'https://iambeta.elm.sa/authservice/authorize?scope=openid&response_type=id_token&response_mode=form_post&client_id=16371621&redirect_uri=https://ezdeal.net/api/v1/home&nonce=b55224f7-e83d-' . $nonce . '-451d32666e59&ui_locales=ar&prompt=login&max_age=' . $time;
@@ -33,17 +34,16 @@ trait Elm
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt( $ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        //curl_setopt($ch, CURLOPT_PORT, 80);
+        //curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        //curl_setopt($ch, CURLOPT_PORT, 81);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
         $responseData = curl_exec($ch);
 
         if (curl_errno($ch)) {
             return curl_error($ch);
-            return curl_getinfo($ch);
-
         }
         curl_close($ch);
         return $responseData;

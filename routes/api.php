f3219da221e47138e49a29b5adaf12fe86633658
fadellabanie <fadel.labanie@gmail.com>
2021-10-20 13:47:52 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -39,11 +39,7 @@ Route::group(['prefix' => 'v1'], function () {
         return Carbon::now()->timestamp;
     });
     
-    Route::post('pay', [PaymentController::class, 'pay']);
-
-    Route::post('response/success', [PaymentController::class, 'success'])->name('response/success');
-    Route::post('response/failure', [PaymentController::class, 'failure'])->name('response/failure');
-
+   
     // Route::get('response/success', function () {
     //     return 'success';
     // })->name('response/success');
@@ -109,6 +105,11 @@ Route::group(['prefix' => 'v1'], function () {
 
         Route::post('un-favorite', [FavoriteController::class, 'unFavorite']);
 
+        Route::post('pay', [PaymentController::class, 'pay']);
+
+        Route::post('response/success', [PaymentController::class, 'success'])->name('response/success');
+        Route::post('response/failure', [PaymentController::class, 'failure'])->name('response/failure');
+    
 
         // Route::apiResource('case', CaseController::class);
 
